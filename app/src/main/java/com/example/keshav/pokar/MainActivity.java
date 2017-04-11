3d41427d0e5e9ccc97a42561074f24abb5e5738c
giteshk11 <writemail2gitesh@gmail.com>
2017-04-11 13:28:59 +0530

    update shared preference

--- app/src/main/java/com/example/keshav/pokar/MainActivity.java
+++ app/src/main/java/com/example/keshav/pokar/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.keshav.pokar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etFirstName, etLastName, etAmount;
+    private Button btnStart;
+    private String mFullName;
+    private int mAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getText();
+                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(getString(R.string.full_name), mFullName);
+                editor.putInt(getString(R.string.amount), mAmount);
+                editor.apply();
+            }
+        });
 
+    }
+
+    private void init() {
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        etLastName = (EditText) findViewById(R.id.etLastName);
+        etAmount = (EditText) findViewById(R.id.etAmount);
+        btnStart = (Button) findViewById(R.id.btnStart);
+    }
+
+    private void getText() {
+        mFullName = etFirstName.getText().toString() + etLastName.getText().toString();
+        mAmount = Integer.parseInt(etAmount.getText().toString());
     }
 }
--- app/src/main/java/com/example/keshav/pokar/ShowChipManupilationActivity.java
+++ app/src/main/java/com/example/keshav/pokar/ShowChipManupilationActivity.java
@@ -1,36 +1,42 @@
 package com.example.keshav.pokar;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class ShowChipManupilationActivity extends AppCompatActivity {
     private static final int red = 5;
-    private static final int blue =25;
-    private static final int green =50;
+    private static final int blue = 25;
+    private static final int green = 50;
     private static final int black = 100;
+    private static int amount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_chip_manupilation);
-
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        amount = sharedPref.getInt(getString(R.string.amount), 0);
+        Toast.makeText(this, String.valueOf(amount), Toast.LENGTH_SHORT).show();
     }
 
-    private void chipCalculation(int amount){
+    private void chipCalculation(int amount) {
 
-        int chipRed,chipBlue,chipGreen,chipBlack;
-        chipBlack=(amount/black);
-        amount = amount%black;
-        chipGreen=(amount/green);
-        amount = amount%green;
-        chipBlue = (amount/blue);
-        amount = amount%blue;
-        chipRed = (amount/red);
+        int chipRed, chipBlue, chipGreen, chipBlack;
+        chipBlack = (amount / black);
+        amount = amount % black;
+        chipGreen = (amount / green);
+        amount = amount % green;
+        chipBlue = (amount / blue);
+        amount = amount % blue;
+        chipRed = (amount / red);
     }
 
-    private int moneyCalculation(int chipRed,int chipBlue,int chipGreen,int chipBlack){
-        int amount=0;
-        amount = (chipRed*red)+(chipBlack*black)+(chipGreen*green)+(chipBlue*blue);
+    private int moneyCalculation(int chipRed, int chipBlue, int chipGreen, int chipBlack) {
+        int amount = 0;
+        amount = (chipRed * red) + (chipBlack * black) + (chipGreen * green) + (chipBlue * blue);
         return amount;
     }
 }
